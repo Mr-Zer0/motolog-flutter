71cc5d4222d6ac486a0f0f94307d020500827410
Yan Naing <tannaing@gmail.com>
2026-05-02 12:56:06 +0700

    build(firebase): integrate Firebase Core for Android
    - Add google-services plugin v4.4.2 to Gradle settings and app
    - Apply google-services plugin in app/build.gradle.kts
    - Add firebase_core dependency to pubspec.yaml
    - Initialize Firebase in main() using generated firebase_options.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'app_colors.dart';
 import 'app_state.dart';
+import 'firebase_options.dart';
 import 'screens/history_screen.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(
     ChangeNotifierProvider(
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_core
 import share_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
 }
